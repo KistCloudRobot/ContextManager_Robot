bb2d80e3fee147afac9a20beceb697b5b3204ec3
uosai <7979us@gmail.com>
2022-09-07 14:56:29 +0900

    로봇 4대 대응을 위한 실행 파일 추가

--- src/test/Lift4_CM.java
+++ src/test/Lift4_CM.java
@@ -1,15 +1,14 @@
 package test;
-
 import kgu.agent.demo.agent.Robot_CM;
 
-public class Lift1_CM {
+public class Lift4_CM {
 	public static void main(String[] args) {
 		String brokerAddress;
 		String robotID;
 		if(args.length == 0) {
-			robotID = "AMR_LIFT1";	
-//			brokerAddress = "tcp://127.0.0.1:61120";
-			brokerAddress = "tcp://172.16.165.141:61116";
+			robotID = "AMR_LIFT4";	
+//			brokerAddress = "tcp://127.0.0.1:61123";
+			brokerAddress = "tcp://172.16.165.141:61113";
 		} else {
 			robotID = args[0];
 			brokerAddress = args[1];
